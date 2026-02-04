6d65296a810241a599e44d10894ec3a5ec9531d4
Chanura04 <chanurakarunanayake12@gmail.com>
2026-02-04 22:52:57 +0530

    update the commands file

--- upgrade_airflow.sh
+++ upgrade_airflow.sh
@@ -5,10 +5,12 @@ docker-compose build --no-cache
 
 
 
-
-
-
-
+pge_url= https://eu-north-1.console.aws.amazon.com/ecr/repositories/private/195322290700/my-dags-repo/_/details?region=eu-north-1
+---for run the docker image from ECR repository---
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 195322290700.dkr.ecr.eu-north-1.amazonaws.com
+docker pull 195322290700.dkr.ecr.eu-north-1.amazonaws.com/my-dags-repo:0.0.1
+docker run 195322290700.dkr.ecr.eu-north-1.amazonaws.com/my-dags-repo:0.0.1
+docker run -p 8080:8080 -it 195322290700.dkr.ecr.eu-north-1.amazonaws.com/my-dags-repo:0.0.1 airflow api-server
 
 
 
